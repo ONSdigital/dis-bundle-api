f13c7dfc2a2fac25572ecf9c6b88fcd72302cf7c
aryan-patel <aryanpatel1771@gmail.com>
2025-05-19 08:42:22 +0100

    lint fix

--- models/pagination.go
+++ models/pagination.go
@@ -2,8 +2,8 @@ package models
 
 // PaginationFields represents the fields used for pagination in an API response
 type PaginationFields struct {
-	Count       int `json:"count"`
-	Limit       int `json:"limit"`
-	Offset      int `json:"offset"`
-	Total_count int `json:"total_count"`
+	Count      int `json:"count"`
+	Limit      int `json:"limit"`
+	Offset     int `json:"offset"`
+	TotalCount int `json:"total_count"`
 }
