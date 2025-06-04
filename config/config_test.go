269ba13965b4bf9a0a719a0b8802886241351409
aryan-patel <aryanpatel1771@gmail.com>
2025-06-04 17:11:50 +0100

    add dataset-api-url config

--- config/config_test.go
+++ config/config_test.go
@@ -20,6 +20,7 @@ func TestSpec(t *testing.T) {
 
 			Convey("The values should be set to the expected defaults", func() {
 				So(cfg.BindAddr, ShouldEqual, ":29800")
+				So(cfg.DatasetAPIURL, ShouldEqual, "http://localhost:22000")
 				So(cfg.GracefulShutdownTimeout, ShouldEqual, 5*time.Second)
 				So(cfg.HealthCheckCriticalTimeout, ShouldEqual, 90*time.Second)
 				So(cfg.HealthCheckInterval, ShouldEqual, 30*time.Second)
