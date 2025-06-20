f2d356d82d853701a685a4a22f36ab6b378b5914
aryan-patel <aryanpatel1771@gmail.com>
2025-06-20 08:24:22 +0100

    create new delete endpoint within API

--- api/api.go
+++ api/api.go
@@ -50,6 +50,10 @@ func Setup(ctx context.Context, cfg *config.Config, router *mux.Router, store *s
 		"/bundles/{bundle-id}/contents",
 		authMiddleware.Require("bundles:create", api.postBundleContents),
 	)
+	api.delete(
+		"/bundles/{bundle-id}/contents/{content-id}",
+		authMiddleware.Require("bundles:delete", api.deleteContentItem),
+	)
 
 	return api
 }
@@ -62,4 +66,9 @@ func (api *BundleAPI) get(path string, handler http.HandlerFunc) {
 // post registers a POST http.HandlerFunc.
 func (api *BundleAPI) post(path string, handler http.HandlerFunc) {
 	api.Router.HandleFunc(path, handler).Methods(http.MethodPost)
+}
+
+// delete registers a DELETE http.HandlerFunc.
+func (api *BundleAPI) delete(path string, handler http.HandlerFunc) {
+	api.Router.HandleFunc(path, handler).Methods(http.MethodDelete)
 }
