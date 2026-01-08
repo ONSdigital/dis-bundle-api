85ed3138120b0af1e7b546a2bc7725ce7ed8724c
aryan-patel <aryanpatel1771@gmail.com>
2026-01-08 15:35:57 +0000

    check policy exists for preview team before creating

--- api/bundles_test.go
+++ api/bundles_test.go
@@ -1187,7 +1187,13 @@ func TestCreateBundle_Success(t *testing.T) {
 			}
 			mockDatasetAPIClient := &datasetAPISDKMock.ClienterMock{}
 			mockPermissionsAPIClient := &permissionsAPISDKMock.ClienterMock{
-				PostPolicyWithIDFunc: func(ctx context.Context, headers permissionsAPISDK.Headers, id string, policy permissionsAPIModels.PolicyInfo) (*permissionsAPIModels.Policy, error) {
+				GetPolicyFunc: func(ctx context.Context, id string, headers permissionsAPISDK.Headers) (*permissionsAPIModels.Policy, error) {
+					if id == "team1" {
+						return &permissionsAPIModels.Policy{}, errors.New("404 Not Found")
+					}
+					return &permissionsAPIModels.Policy{}, nil
+				},
+				PostPolicyWithIDFunc: func(ctx context.Context, id string, policy permissionsAPIModels.PolicyInfo, headers permissionsAPISDK.Headers) (*permissionsAPIModels.Policy, error) {
 					return nil, nil
 				},
 			}
@@ -1216,8 +1222,9 @@ func TestCreateBundle_Success(t *testing.T) {
 				So(w.Header().Get("Cache-Control"), ShouldEqual, "no-store")
 				So(w.Header().Get("ETag"), ShouldEqual, "some-etag")
 			})
-			Convey("And a policy should be created for each preview team", func() {
-				So(len(mockPermissionsAPIClient.PostPolicyWithIDCalls()), ShouldEqual, len(*validBundle.PreviewTeams))
+			Convey("And a policy should be created for any non-existing preview teams", func() {
+				So(len(mockPermissionsAPIClient.GetPolicyCalls()), ShouldEqual, 2)
+				So(len(mockPermissionsAPIClient.PostPolicyWithIDCalls()), ShouldEqual, 1)
 			})
 		})
 	})
@@ -1255,11 +1262,7 @@ func TestCreateBundleNoPreviewTeam_Success(t *testing.T) {
 				},
 			}
 			mockDatasetAPIClient := &datasetAPISDKMock.ClienterMock{}
-			mockPermissionsAPIClient := &permissionsAPISDKMock.ClienterMock{
-				PostPolicyWithIDFunc: func(ctx context.Context, headers permissionsAPISDK.Headers, id string, policy permissionsAPIModels.PolicyInfo) (*permissionsAPIModels.Policy, error) {
-					return nil, nil
-				},
-			}
+			mockPermissionsAPIClient := &permissionsAPISDKMock.ClienterMock{}
 			bundleAPI := GetBundleAPIWithMocks(store.Datastore{Backend: mockedDatastore}, mockDatasetAPIClient, mockPermissionsAPIClient, false)
 			bundleAPI.Router.ServeHTTP(w, r)
 
@@ -1704,7 +1707,11 @@ func TestCreateBundle_Failure_CheckBundleExistsByTitleFails(t *testing.T) {
 			mockDatasetAPIClient := &datasetAPISDKMock.ClienterMock{}
 
 			mockPermissionsAPIClient := &permissionsAPISDKMock.ClienterMock{
-				PostPolicyWithIDFunc: func(ctx context.Context, headers permissionsAPISDK.Headers, id string, policy permissionsAPIModels.PolicyInfo) (*permissionsAPIModels.Policy, error) {
+				GetPolicyFunc: func(ctx context.Context, id string, headers permissionsAPISDK.Headers) (*permissionsAPIModels.Policy, error) {
+					// Policy must not exist to trigger creation
+					return &permissionsAPIModels.Policy{}, errors.New("404 Not Found")
+				},
+				PostPolicyWithIDFunc: func(ctx context.Context, id string, policy permissionsAPIModels.PolicyInfo, headers permissionsAPISDK.Headers) (*permissionsAPIModels.Policy, error) {
 					return nil, errors.New("failed to create policy")
 				},
 			}
--- application/policies.go
+++ application/policies.go
@@ -2,6 +2,9 @@ package application
 
 import (
 	"context"
+	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/ONSdigital/dis-bundle-api/apierrors"
 	"github.com/ONSdigital/dis-bundle-api/models"
@@ -21,6 +24,14 @@ func (s *StateMachineBundleAPI) CreateBundlePolicies(ctx context.Context, authTo
 	}
 
 	for _, team := range *previewTeams {
+		policyExists, err := s.CheckPolicyExists(ctx, authToken, team.ID)
+		if err != nil {
+			return err
+		}
+		if policyExists {
+			continue
+		}
+
 		policyInfo := permissionsAPIModels.PolicyInfo{
 			Entities: []string{
 				"groups/" + team.ID,
@@ -28,11 +39,25 @@ func (s *StateMachineBundleAPI) CreateBundlePolicies(ctx context.Context, authTo
 			Role: role.String(),
 		}
 
-		_, err := s.PermissionsAPIClient.PostPolicyWithID(ctx, permissionsAPISDK.Headers{Authorization: authToken}, team.ID, policyInfo)
+		_, err = s.PermissionsAPIClient.PostPolicyWithID(ctx, team.ID, policyInfo, permissionsAPISDK.Headers{Authorization: authToken})
 		if err != nil {
 			return err
 		}
 	}
 
 	return nil
+}
+
+// CheckPolicyExists checks if a policy with the given ID exists
+func (s *StateMachineBundleAPI) CheckPolicyExists(ctx context.Context, authToken, policyID string) (bool, error) {
+	_, err := s.PermissionsAPIClient.GetPolicy(ctx, policyID, permissionsAPISDK.Headers{Authorization: authToken})
+	if err != nil {
+		// Permissions API will return an error containing "404" if the policy is not found
+		// Note: The Permissions API SDK does not currently provide an alternative way to check for API response codes
+		if strings.Contains(err.Error(), strconv.Itoa(http.StatusNotFound)) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
